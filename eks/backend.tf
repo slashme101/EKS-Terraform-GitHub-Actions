ab6e79690c3373ff9e64c64fb71f368142059041
Rodney <slashme101@yahoo.com>
2025-06-03 09:53:07 +0100

    changed backend bucket

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "terraform-state-dove123"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    #dynamodb_table = "Lock-Files"
     encrypt        = true
   }
 }
